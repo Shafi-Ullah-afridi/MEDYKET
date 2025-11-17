aa2cd70cebb0dc05ee8e1751f38f03f2e5b0036b
Shafi-Ullah-afridi <shafiullahafridi62@gmail.com>
2025-11-17 11:19:32 +0500

    one comment in server file

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // ===== Database & Cloudinary Connections =====
+// i write one comments to   check that it is working or not
 connectDB();
 connectCloudinary();
 
